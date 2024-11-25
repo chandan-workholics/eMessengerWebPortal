19d2120a37cf0ff42ce6dc3d3065da8426e6442e
HP <chandan.workholics@gmail.com>
2024-11-25 18:35:07 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import Chat from './pages/Chat';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/eMessenger/">
     <Routes>
       <Route path="/" element={<App />}>
         <Route path='/' element={<SignIn />} />
